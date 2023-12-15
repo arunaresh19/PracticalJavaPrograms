6d9c536d204bd9b4406108ea2d5d075aa4f9a260
dell <dell@DESKTOP-PPLHQL8>
2023-12-15 22:20:18 +0530

    commit on 15.12.2023

---
+++ project1/src/arthemeticoperators/arthemeticop.java
@@ -0,0 +1,10 @@
+package arthemeticoperators;
+
+public class arthemeticop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Arthemetic operations");
+	}
+
+}
---
+++ project1/src/operators/add.java
@@ -0,0 +1,24 @@
+package operators;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=5;
+		System.out.println("a+b="+(a+b));
+
+		a=4;
+		b=2;
+		System.out.println("a-b="+(a-b));
+
+		a=5;
+		b=3;
+		System.out.println("a*b="+(a*b));
+
+		a=8;
+		b=4;
+		System.out.println("a/b="+(a/b));
+	}
+
+}
